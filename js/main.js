2c53cd23c6af2fa57fe69f0965bfde865c90d7c8
DAKRFOX <relibarion@gmail.com>
2021-08-02 01:50:02 -0500

    first

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var typed = new Typed('.animate', {
+    strings: [
+        "<i class='imma'>a developer.</i>",
+        "<i class='imma'>a designer.</i>",
+        "<i class='imma'>an ethical hacker.</i>",
+        "<i class='imma'>an entrepreneur.</i>"
+    ],
+        typeSpeed: 60,
+        startDelay: 280,
+        backSpeed: 60,
+        backDelay: 1500,
+        loop: true
+  });
